a0ffe73d2a593cba1f5fd0594787c136d0df3315
Mahdi Asali <37123788+Mahdi-Asaly@users.noreply.github.com>
2021-09-15 22:27:26 +0300

    updating email generator

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,46 @@
 import "./contact.scss"
 import {useState} from "react";
+import emailjs from "emailjs-com";
 export default function Contact() {
-    const [message,setMessage] = useState(false);
-    const handleSubmit= (e)=>{
-        e.preventDefault();
-        setMessage(true);
-    }
-    const handleClear = (e)=>{
-        console.log("here in clear")
-        document.getElementById("email").value="";
-        document.getElementById("message").value="";
 
+    let [name, setName] = useState('');
+    let [email, setEmail] = useState('');
+    let [subject, setSubject] = useState('');
+
+
+    const handleChange = (event) => {
+        setName(event.target.value);
     }
+    const handleEmail = (event) => {
+        setEmail(event.target.value);
+    }
+    const handleSubject = (event) => {
+        setSubject(event.target.value);
+    }
+         
+
+
+    function sendEmail(e) {
+        if (!name || !email) {
+            console.log("name empty");
+             e.preventDefault();
+             return;
+         }
+
+        e.preventDefault();
+        emailjs.sendForm('service_u6nmfpd', 'template_9ahli7q', e.target, 'user_uXHw7zQ0Y4gwNQEL1YyNI')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+        e.target.reset();
+        setMessage(true);
+      }
+
+    const [message,setMessage] = useState(false);
+
+
     return (
         <div className="contact" id="contact">
             <div className="left">
@@ -19,10 +48,13 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact Me</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" id="email" placeholder="Email"/>
-                    <textarea id="message" placeholder="Message"></textarea>
-                    <button type="submit" id="submit" className="btn btn-primary" onClick={handleClear}>Send</button>
+                <form id="form-g" onSubmit={sendEmail}>
+                    <input type="text" id="name" className="form-control" placeholder="Name" value={name} onChange={handleChange} name="name"/>
+                    <input type="email" id="email" className="form-control" placeholder="Email Address" value={email} onChange={handleEmail} name="email"/>
+                    <input type="text" id="subject" className="form-control" placeholder="Subject" value={subject} onChange={handleSubject} name="subject"/>
+                    <textarea className="form-control" id="message" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                    <input type="submit" className="button" value="Send Message"></input>
+
                     {message &&
                      <span>Thanks, I will review your message and reply ASAP !</span>
                     }

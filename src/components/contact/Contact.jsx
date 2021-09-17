892ce888bd09ca13060b30a5b300245dd94082b9
Mahdi Asali <37123788+Mahdi-Asaly@users.noreply.github.com>
2021-09-17 20:44:03 +0300

    added updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -22,11 +22,10 @@ export default function Contact() {
 
     function sendEmail(e) {
         if (!name || !email) {
-            console.log("name empty");
              e.preventDefault();
+             alert('one or more fields are empty :(');
              return;
          }
-
         e.preventDefault();
         emailjs.sendForm('service_u6nmfpd', 'template_9ahli7q', e.target, 'user_uXHw7zQ0Y4gwNQEL1YyNI')
           .then((result) => {
@@ -47,16 +46,20 @@ export default function Contact() {
                 <img src="assets/shake.svg" alt=""/>
             </div>
             <div className="right">
-                <h2>Contact Me</h2>
+               <h2>Contact </h2>
+                <h3>Do you have a business and wants to</h3>
+                <h3> publish it to the world? 
+                    I'm here to help you! </h3>
                 <form id="form-g" onSubmit={sendEmail}>
                     <input type="text" id="name" className="form-control" placeholder="Name" value={name} onChange={handleChange} name="name"/>
                     <input type="email" id="email" className="form-control" placeholder="Email Address" value={email} onChange={handleEmail} name="email"/>
                     <input type="text" id="subject" className="form-control" placeholder="Subject" value={subject} onChange={handleSubject} name="subject"/>
                     <textarea className="form-control" id="message" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
                     <input type="submit" className="button" value="Send Message"></input>
-
                     {message &&
-                     <span>Thanks, I will review your message and reply ASAP !</span>
+                     <span>Thanks, I will review your message and reply ASAP ! 
+                         If you still have questions please call me at : 052-3469716
+                     </span>
                     }
                </form>
             </div>
--- src/data.js
+++ src/data.js
@@ -5,26 +5,21 @@ export const webPortfolio = [
     img:"https://i.ytimg.com/vi/L-sChoW4eV0/maxresdefault.jpg",
     gitUrl: "https://github.com/Mahdi-Asaly/deer_manager_csharp_mvc"
 },
+
 {
     id:2,
-    title: "Deer Manager React",
-    img:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
-    gitUrl: "https://github.com/Mahdi-Asaly/sheep_manager_react"
-},
-{
-    id:3,
     title: "Online Shop with React & Node.JS",
     img:"https://res.cloudinary.com/practicaldev/image/fetch/s--9m3CjzLk--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/fyzf0no5ej1fgxp5972e.png",
     gitUrl:"https://github.com/Mahdi-Asaly/online-linux-app"
 },
 {
-    id:4,
+    id:3,
     title: "Payment service C#",
     img:"https://business.ebanx.com/hubfs/PE-PSP-actors-PSP-1.png",
     gitUrl:"https://github.com/Mahdi-Asaly/payment_service_ASP.NET"
 },
 {
-    id:5,
+    id:4,
     title: "PC Store MVC .NET",
     img:"https://buildify.cc/wp-content/uploads/2017/03/Computer-Store-PrestaShop-Theme_547.jpg",
     gitUrl:"https://github.com/Mahdi-Asaly/pc_store_csharp_mvc"
@@ -69,6 +64,5 @@ export const deployingPortfolio =  [
         title: "my Portfolio",
         img:"https://d3kqdc25i4tl0t.cloudfront.net/articles/content/543_378726_tech.hero.jpg",
         gitUrl:"https://github.com/Mahdi-Asaly/portfolio"
-  
     },
 ]
